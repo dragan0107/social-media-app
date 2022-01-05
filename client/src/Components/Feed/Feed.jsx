d3a08be1f1715929bdfcc3d373c160715db9f24b
drip016 <73792907+drip016@users.noreply.github.com>
2022-01-05 01:48:17 +0100

    updating components

--- client/src/Components/Feed/Feed.jsx
+++ client/src/Components/Feed/Feed.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import SharePost from '../SharePost/SharePost';
 import './Feed.css';
 
 const Feed = () => {
-    return <div className="feed">Feed</div>;
+    return (
+        <div className="feed">
+            <div className="feed-wrapper">
+                <SharePost />
+            </div>
+        </div>
+    );
 };
 
 export default Feed;
---
+++ client/src/Components/SharePost/SharePost.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './SharePost.css';
+
+const SharePost = () => {
+    return (
+        <div className="share-post">
+            <div className="share-wrapper">
+                <div className="share-top">
+                    <img
+                        src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                        alt=""
+                        className="user-pfp"
+                    />
+                    {/* <input
+                        type="text"
+                        placeholder="What's on your mind Drip?"
+                    /> */}
+                    <textarea
+                        name=""
+                        id="status-textbox"
+                        cols="50"
+                        rows="2"
+                        placeholder="What's on your mind Drip?"
+                        maxlength="280"
+                        autoFocus
+                    ></textarea>
+                </div>
+                <hr className="separator-line" />
+                <div className="share-bottom">
+                    <div className="add-media">
+                        <label htmlFor="media-input">
+                            <i className="media-icon fas fa-photo-video"></i>
+                        </label>
+                        <span className="media-span">Add Photo or Video</span>
+                        <input type="file" name="" id="media-input" />
+                    </div>
+                    <button className="share-btn">Share</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SharePost;
