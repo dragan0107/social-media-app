597ebbb5e8037614c3293d7ec3384642f0f7b5ee
drip016 <73792907+drip016@users.noreply.github.com>
2022-02-01 23:39:44 +0100

    added a topbar menu for profile and settings

--- client/src/Components/Topbar/Topbar.jsx
+++ client/src/Components/Topbar/Topbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthContext';
 import './Topbar.css';
-
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Settings from '@mui/icons-material/Settings';
 const Topbar = () => {
     const { user, dispatch } = useContext(AuthContext);
+    const [showTopMenu, setShowTopMenu] = useState(false);
 
     const handleClick = () => {
         localStorage.removeItem('jwt');
@@ -16,68 +19,95 @@ const Topbar = () => {
     const notifications = 8;
 
     return (
-        <div className="topbar">
-            <div className="left">
-                <h1 className="brand-name">Friendbook</h1>
-            </div>
-            <div className="center">
-                <input type="text" placeholder="Search" />
-                <Link to="/">
-                    <a href="">Homepage</a>
-                </Link>
+        <header>
+            <navbar className="topbar">
+                <div className="left">
+                    <h1 className="brand-name">Friendbook</h1>
+                </div>
+                <div className="center">
+                    <input type="text" placeholder="Search" />
+                    <Link to="/">
+                        <a href="">Homepage</a>
+                    </Link>
 
-                {/* <a href="">Timeline</a> */}
-                <a className="logout-link" onClick={handleClick}>
-                    Log Out
-                </a>
-            </div>
-            <div className="right">
-                <div className="info">
-                    <div className="info-right">
-                        <div
-                            className={`notification ${
-                                friendReq > 0 ? 'white-notification' : ''
-                            } `}
-                        >
-                            <i class="fas fa-user-friends"></i>
-                            {friendReq > 0 && (
-                                <span className="count">{friendReq}</span>
-                            )}
+                    {/* <a href="">Timeline</a> */}
+                    <a className="logout-link" onClick={handleClick}>
+                        Log Out
+                    </a>
+                </div>
+                <div className="right">
+                    <div className="info">
+                        <div className="info-right">
+                            <div
+                                className={`notification ${
+                                    friendReq > 0 ? 'white-notification' : ''
+                                } `}
+                            >
+                                <i class="fas fa-user-friends"></i>
+                                {friendReq > 0 && (
+                                    <span className="count">{friendReq}</span>
+                                )}
+                            </div>
+                            <div
+                                className={`notification ${
+                                    messages > 0 ? 'white-notification' : ''
+                                } `}
+                            >
+                                <i class="fas fa-envelope"></i>
+                                {messages > 0 && (
+                                    <span className="count">{messages}</span>
+                                )}
+                            </div>
+                            <div
+                                className={`notification ${
+                                    notifications > 0
+                                        ? 'white-notification'
+                                        : ''
+                                } `}
+                            >
+                                <i class="fas fa-bell"></i>
+                                {notifications > 0 && (
+                                    <span className="count">
+                                        {notifications}
+                                    </span>
+                                )}
+                            </div>
                         </div>
-                        <div
-                            className={`notification ${
-                                messages > 0 ? 'white-notification' : ''
-                            } `}
-                        >
-                            <i class="fas fa-envelope"></i>
-                            {messages > 0 && (
-                                <span className="count">{messages}</span>
-                            )}
-                        </div>
-                        <div
-                            className={`notification ${
-                                notifications > 0 ? 'white-notification' : ''
-                            } `}
-                        >
-                            <i class="fas fa-bell"></i>
-                            {notifications > 0 && (
-                                <span className="count">{notifications}</span>
+                        <div className="topbar-menu">
+                            <img
+                                className="profile-pic"
+                                src={
+                                    user.profilePic ||
+                                    'https://res.cloudinary.com/dripcloud/image/upload/v1642120967/test_upload_react/facebook-default-no-profile-pic1_wq7ysr.jpg'
+                                }
+                                alt=""
+                                onClick={() =>
+                                    setShowTopMenu((prevValue) => !prevValue)
+                                }
+                            />
+                            {showTopMenu && (
+                                <div className="menu-box">
+                                    <ul className="option-list">
+                                        <li>
+                                            {' '}
+                                            <AccountCircleIcon />{' '}
+                                            <Link
+                                                to={`/profile/${user.username}`}
+                                            >
+                                                <span>Profile</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Settings /> <span>Settings</span>
+                                        </li>
+                                    </ul>
+                                </div>
                             )}
                         </div>
                     </div>
-                    <Link to={`/profile/${user.username}`}>
-                        <img
-                            className="profile-pic"
-                            src={
-                                user.profilePic ||
-                                'https://res.cloudinary.com/dripcloud/image/upload/v1642120967/test_upload_react/facebook-default-no-profile-pic1_wq7ysr.jpg'
-                            }
-                            alt=""
-                        />
-                    </Link>
                 </div>
-            </div>
-        </div>
+            </navbar>
+        </header>
     );
 };
 
